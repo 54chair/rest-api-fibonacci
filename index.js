0bbe26e7543687bd23c383bb9252d2e73751dc02
54chair <takukoshi3721@gmail.com>
2023-01-25 23:22:22 +0900

    バリデーションの実装

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const JSONbig = require('json-bigint');
+const { check, validationResult } = require('express-validator');
 
-app.get('/fib', function (req, res) {
-  var n = req.query.n;
-  var fibobacci = fib(n);
-  var result = JSONbig.stringify({"result": fibobacci}, null, 2);
-  res.header('Content-Type', 'application/json; charset=utf-8')
-  res.status(200).send(result);
+app.get('/fib', [
+  check('n').isInt({min: 1})
+  ], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    var result = JSON.stringify({"status": 400, "message": "Bad request."}, null, 2);
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.status(400).send(result);
+  } else {
+    var n = req.query.n;
+    var fibobacci = fib(n);
+    var result = JSONbig.stringify({"result": fibobacci}, null, 2);
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.status(200).send(result);
+  }
 })
 
 function fib(n) {
