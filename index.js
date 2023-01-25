887ec7fc6efa289a02f42ad19a04549ff5b26964
54chair <takukoshi3721@gmail.com>
2023-01-25 22:26:45 +0900

    フィボナッチ数を返す処理の実装

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+const JSONbig = require('json-bigint');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/fib', function (req, res) {
+  var n = req.query.n;
+  var fibobacci = fib(n);
+  var result = JSONbig.stringify({"result": fibobacci}, null, 2);
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.status(200).send(result);
 })
 
-app.listen(3000)
+function fib(n) {
+  const result = [0, 1];
+
+  for (i = 2; i <= n; i++) {
+    const a = BigInt(result[i - 1]);
+    const b = BigInt(result[i - 2]);
+
+    result.push(a + b);
+  }
+  return result[n];
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+  console.log("listening server")
+})
